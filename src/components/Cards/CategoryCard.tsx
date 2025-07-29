54e7385fd2ea0eb89cb11e33a6d7b1406ebfaa61
Your Name <ee.marishkin@yandex.ru>
2025-07-29 20:07:25 +0400

    FilterCategory+

--- src/components/Cards/CategoryCard.tsx
+++ src/components/Cards/CategoryCard.tsx
@@ -2,6 +2,7 @@ import type { FC } from "react";
 import type { ICardCategory } from "../../types";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../../styles/CategoryCard.css'
+import { faBox } from '@fortawesome/free-solid-svg-icons'; 
 
 interface CategoryCardProps {
     category: ICardCategory
@@ -10,7 +11,7 @@ interface CategoryCardProps {
 export const CategoryCard: FC<CategoryCardProps> = ({ category }) => {
     return (
         <div className="category-card">
-            <FontAwesomeIcon icon={category.icon} className="category-icon" />
+            <FontAwesomeIcon icon={category.icon || faBox} className="category-icon" />
             <span className="category-name">{category.name}</span>
         </div>
     )
